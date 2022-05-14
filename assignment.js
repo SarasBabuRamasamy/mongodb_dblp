e2999260f638d6be66a2e41b062422a5fec2179f
SarasBabuRamasamy <98818815+SarasBabuRamasamy@users.noreply.github.com>
2022-05-15 00:15:41 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -8,3 +8,27 @@ db.publis.distinct('publisher')
 db.publis.createIndex({'title':'text'})
 
 db.publis.find({$text:{$search:'database'}})
+
+//#mapreduce to find the number of publications by an author
+
+db.runCommand({
+mapReduce: 'publis',
+map: function(){
+    for (let index = 0; index < this.authors.length;++index) 
+    {
+        let author=this.authors[ index ];
+        emit(author,1);
+    }
+},
+reduce: function(author, counters){
+    count = 0;
+    for (let index=0; index <counters.length; ++index)
+    {
+        count +=counters[index];
+    }
+    return count;
+},
+out: {inline: 1}
+}
+)
+
