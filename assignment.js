05db0862712cd789d6449c28b4aa972b7de89c96
SarasBabuRamasamy <98818815+SarasBabuRamasamy@users.noreply.github.com>
2022-05-15 00:23:51 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,13 +1,28 @@
+//1. List of all books.  
 db.publis.find({type:'Book'})
+
+//2. List of publications since 2012.  
 db.publis.find({year:{$gt:2012}})
+
+//3. List of publications by the author “Massimo Zancanaro”. 
 db.publis.find({authors:'Massimo Zancanaro'})
+
+//4. Count the number of “Massimo Zancanaro publications. 
 db.publis.count({authors:'Massimo Zancanaro'})
+
+//5. Sort “Massimo Zancanaro” publications by book title and start page.  
 db.publis.find({authors:'Massimo Zancanaro'}).sort({'booktitle':1})
 db.publis.find({authors:'Massimo Zancanaro'}).sort({'pages.start':1})
-db.publis.distinct('publisher')
-db.publis.createIndex({'title':'text'})
 
+//6. List of all distinct publishers.  
+db.publis.distinct('publisher')
+
+//7. List of all publications which title contains the word “database”.
+db.publis.createIndex({'title':'text'})
+//all the values of the field 'title' is text indexed
 db.publis.find({$text:{$search:'database'}})
+//after indexing, the field title is text searched for the word 'database'
+
 
 //#mapreduce to find the number of publications by an author
 
